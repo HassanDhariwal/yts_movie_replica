b434b0bc1829dc5efa28c97f24b58b8fdd227f61
Hassan <hassanriaz262@gmail.com>
2023-10-11 19:35:50 +0500

    [BB-10] Learn Hash and use the hash to Replace the name and all other details

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   def index
-    @movies =["Batman" , "Spiderman ", "Dedpol", "Ironman"]
+    @movies = [
+      {:title=> 'Batman'    , :year=> 2023,    :rating=> '6.7 / 10', :type=> 'Action',  :image_path=> 'marcin.jpg' },
+      {:title=> 'Spiderman' , :year=> 2023,    :rating=> '6.7 / 10', :type=> 'Funny',   :image_path=> 'spiderman1.jpg' },
+      {:title=> 'Dedpol'    , :year=> 2023,    :rating=> '6.7 / 10', :type=> 'Darama',  :image_path=> 'dedpol.jpg' },
+      {:title=> 'Ironman'   , :year=> 2023,    :rating=> '6.7 / 10', :type=> 'Sci',     :image_path=> 'emmanuel.jpg' }
+    ]
   end
 end
