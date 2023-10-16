86e08ef77bc926e7970cb07c95dd25a93e9e8a62
Hassan <hassanriaz262@gmail.com>
2023-10-16 22:24:52 +0500

    [BB-9] Create one function that will reflect the HTML and JSON Both

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,11 @@ class MoviesController < ApplicationController
   def index
 
     @movies = YAML.load_file(Rails.root.join('config', 'movie.yml'))
+
+    respond_to do |f|
+      f.html
+      f.json{ render json: @movies}
+    end
+
   end
 end
