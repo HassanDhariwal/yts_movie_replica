ecc452ac4bfbf963a9d85f99d8695f136fec59b8
Hassan <hassanriaz262@gmail.com>
2023-10-17 11:05:59 +0500

    [BB-9] show title year and rating with json

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,18 @@ class MoviesController < ApplicationController
   def index
 
     @movies = YAML.load_file(Rails.root.join('config', 'movie.yml'))
-
-    respond_to do |f|
-      f.html
-      f.json{ render json: @movies}
+    only_name_year = []
+    @movies.each do |a|
+      a.each do |movie|
+        only_name_year << {title: movie['title'], year:movie['year'],rating:movie['rating']}
+       end
     end
 
+    respond_to do |formate|
+      formate.html
+      # formate.json{ render json: @movies.map { |movie| movie['title']  }}
+      formate.json { render json: only_name_year }
+    end
   end
+
 end
