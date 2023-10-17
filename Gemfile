5148ac44268f26bb7fb0741443dd4e5573943d86
Hassan <hassanriaz262@gmail.com>
2023-10-17 23:37:51 +0500

    [BB-9] use yml to create array

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'pry', '~> 0.10.3'
+gem 'awesome_print'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,14 @@
 class MoviesController < ApplicationController
   def index
+    # binding.pry
 
     @movies = YAML.load_file(Rails.root.join('config', 'movie.yml'))
+
     only_name_year = []
+    i = 1
     @movies.each do |a|
+      i = i+1
+      puts "row no is #{i}"
       a.each do |movie|
         only_name_year << {title: movie['title'], year:movie['year'],rating:movie['rating']}
        end
@@ -15,5 +20,4 @@ class MoviesController < ApplicationController
       formate.json { render json: only_name_year }
     end
   end
-
-end
+  end
