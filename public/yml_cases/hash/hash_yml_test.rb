cf5b050eeb15d8ee26564e3b9da2b9f0222dcfef
Hassan <hassanriaz262@gmail.com>
2023-10-18 17:27:04 +0500

    [BB-12] ruby only : hash from yml and hash of int values  and hash of string values are separate

--- public/yml_cases/hash/hash_yml_test.rb
+++ public/yml_cases/hash/hash_yml_test.rb
@@ -2,24 +2,28 @@ require 'yaml'
 require 'pry'
 
 sum = 0
-str = {}
+str_hash = {}
+int_hash = {}
 
      yml_data = YAML.load_file('hash.yml')
       	puts "---------HASH-----------"
      yml_data.each do |key, value |
      	puts "key and value is :#{key} => #{value} "
-     	# binding.pry
      	if value.instance_of?(String)
-     		str [key] = value
+     		str_hash [key] = value
      	elsif value.instance_of?(Integer)
      		puts "#{value}"
-     		# binding.pry
+     		int_hash [key] = value
     		sum = sum+value
      	end
  	end
  	puts "---------SUM-----------"
- 	puts "sum of hash value is #{sum}"
+ 	puts " Sum  hash values is = #{sum}"
  	puts "---------STR-----------"
- 	puts "#{str}"
+
+ 	puts " Sting hash value: #{str_hash}"
+ 	puts "---------INT-----------"
+ 	puts " Integer hash value: #{int_hash}"
+
 
 
