6a561b7be6d37f1b185ad321df095a9d5778576e
Hassan <hassanriaz262@gmail.com>
2023-10-20 00:03:47 +0500

    [BB-12] [BB-14] ruby only : nested Array in hash  from yml and  get date from user show date,day with month name

---
+++ public/yml_cases/nested_array/nested_array_yml_test.rb
@@ -0,0 +1,36 @@
+require 'yaml'
+require 'pry'
+require 'awesome_print'
+
+yml_data = YAML.load_file('nested_array.yml')
+
+month_name = yml_data.keys[0]
+
+array_data = yml_data['february_2023']
+
+days_name_data = array_data[0]
+
+
+# 1st way days_array = array_data.drop(1)
+puts "Enter a date "
+user_input_date = gets.to_i
+
+
+value_index = nil
+
+array_data.each do |array|
+	value_index  = array.find_index(user_input_date)
+
+	if value_index
+		break
+	end
+
+end
+
+puts "outside loop"
+ # 2nd way day_name  = days_name_data[value_index]
+ day_name = array_data[0][value_index]
+
+ puts "Date :#{user_input_date} Day name : #{day_name} , Month #{month_name} "
+
+
