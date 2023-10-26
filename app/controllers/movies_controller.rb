e9bc9cce973645005aead0c654f76c04bd635cac
Hassan <hassanriaz262@gmail.com>
2023-10-26 11:28:07 +0500

    BB-9 show movies recode in jason by row numbers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,29 @@
 class MoviesController < ApplicationController
   def index
+
+
+
     @movies = YAML.load_file(Rails.root.join('config', 'movie.yml'))
 
-    only_name_year = []
+
+    row_vise_recode= []
     i = 1
-    @movies.each do |a|
-      i = i+1
-      puts "row no is #{i}"
-      a.each do |movie|
-        only_name_year << {title: movie['title'], year:movie['year'],rating:movie['rating']}
-       end
+
+    @movies.each do |row|
+
+      movies_recode = []
+
+      row.each do |movie|
+          movies_recode << {title: movie['title'], year:movie['year'],rating:movie['rating']}
+      end
+      row_vise_recode  << { "row_#{i}" => movies_recode }
+        i = i+1
     end
+
 
     respond_to do |formate|
       formate.html
-      formate.json { render json: only_name_year }
+      formate.json { render json: row_vise_recode }
     end
   end
-  end
+end
